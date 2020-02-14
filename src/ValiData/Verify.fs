229e2daa5c0f4f2afe3fd1d6699b927cfece7139
Kasey Speakman <kasey@cornerspeed.com>
2020-02-14 16:30:59 -0600

    fixed formatting

--- src/ValiData/Verify.fs
+++ src/ValiData/Verify.fs
@@ -5,37 +5,31 @@ module Verify =
 
     let verify (x: 'T) (r: 'T -> Result<'U, List<'TError>>) (fr: Result<'U -> 'V, List<'TError>>) =
         match fr, r x with
-            | Ok fOk, Ok ok ->
-                Ok (fOk ok)
-
-            | Ok _, Error err ->
-                Error err
-
-            | Error errf, Ok _ ->
-                Error errf
-
-            | Error errf, Error err ->
-                // ehh, not very efficient
-                Error (List.append errf err)
+        | Ok fOk, Ok ok ->
+            Ok (fOk ok)
+        | Ok _, Error err ->
+            Error err
+        | Error errf, Ok _ ->
+            Error errf
+        | Error errf, Error err ->
+            // ehh, not very efficient
+            Error (List.append errf err)
 
 
     let fail err =
-        Error (List.singleton err)
+        Error [err]
 
 
     let assert_ (x: 'x) (r: 'x -> Result<_, List<'TError>>) (fr: Result<'T -> 'U, List<'TError>>) =
         match fr, r x with
-            | Ok _, Ok _ ->
-                fr
-
-            | Ok _, Error err ->
-                Error err
-
-            | Error errf, Ok _ ->
-                Error errf
-
-            | Error errf, Error err ->
-                Error (List.append errf err)
+        | Ok _, Ok _ ->
+            fr
+        | Ok _, Error err ->
+            Error err
+        | Error errf, Ok _ ->
+            Error errf
+        | Error errf, Error err ->
+            Error (List.append errf err)
 
 
     let (>=>) f g =
